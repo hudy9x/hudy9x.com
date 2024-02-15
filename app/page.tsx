931cf9909c2d46a9c40b2a135e13c2a806b36098
hudy9x <hudy9x@gmail.com>
2024-02-15 15:24:37 +0700

    add a new brand animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,30 @@ import Link from "next/link";
 import React from "react";
 import Particles from "./components/Particles";
 import Image from "next/image";
+import { BrandName } from "./components/BrandName";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
 ];
 
-const words = 'hudy9x'.split('')
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <Image className="rounded-full w-[70px] h-[70px] sm:w-[100px] sm:h-[100px] grayscale transition-all duration-700 hover:grayscale-0 animate-fade-in" src={'https://avatars.githubusercontent.com/u/95471659?v=4'} alt="Hudy9x" width={100} height={100} />
-      <nav className="my-16 animate-fade-in">
+      <Image
+        className="rounded-full w-[65px] h-[65px] sm:w-[100px] sm:h-[100px] grayscale transition-all duration-700 hover:grayscale-0 animate-fade-in"
+        src={"https://avatars.githubusercontent.com/u/95471659?v=4"}
+        alt="Hudy9x"
+        width={100}
+        height={100}
+      />
+      <nav className="my-12 sm:my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="uppercase duration-500 text-zinc-500 hover:text-zinc-300"
+              className="uppercase text-xs sm:text-base duration-500 text-zinc-500 hover:text-zinc-300"
             >
               {item.name}
             </Link>
@@ -31,33 +36,24 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in-1000"
         quantity={100}
       />
-      <div>
-        <h1 className=" z-10 font-extrabold text-5xl text-transparent duration-1000 bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-          {words.map((w, widx) => {
-            const extra = widx ? widx * 0.4 : 0
-            const duration = 3 + extra + 's'
-
-            return <span className="animate-title" style={{ animationDuration: duration }} key={w}>{w}</span>
-          })}
-        </h1>
-
-      </div>
+      <BrandName />
 
       <div className="my-16 text-center animate-fade-in mx-10 space-y-3 sm:mx-0 text-sm sm:text-xl">
-        <h2 className="text-zinc-500 ">I speak web languages. Turning ideas into apps for customers and myself. Always coding, always learning.</h2>
+        <h2 className="text-zinc-500 ">
+          I speak web languages. Turning ideas into apps for customers and
+          myself. Always coding, always learning.
+        </h2>
         <h2 className="text-zinc-500 ">
           {"I'm building "}
           <Link
-
             href="/projects/namviek"
             className="underline duration-500 hover:text-zinc-300"
           >
             namviek.com
-          </Link> to help small startups managing tasks with points easily.
+          </Link>{" "}
+          to help small startups managing tasks with points easily.
         </h2>
       </div>
     </div>
   );
-
 }
-
