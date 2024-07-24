58f2445563ae2c20868cdd11fd12508e5a6640d3
hudy@mac-air <hudy9x@gmail.com>
2024-07-24 17:21:30 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Github } from "lucide-react";
+import { Dock, DockIcon } from "./_components/Dock";
 import About from "./_containers/About";
 import GetInTouch from "./_containers/GetInTouch";
 import Header from "./_containers/Header";
@@ -16,6 +18,22 @@ export default function Home() {
       <Mywork />
       <WorkList />
       <GetInTouch />
+      <div className="fixed bottom-4 left-1/2 -translate-x-1/2">
+        <Dock direction="middle">
+          <DockIcon>
+            <Github size={24} />
+          </DockIcon>
+          <DockIcon>
+            <Github size={24} />
+          </DockIcon>
+          <DockIcon>
+            <Github size={24} />
+          </DockIcon>
+          <DockIcon>
+            <Github size={24} />
+          </DockIcon>
+        </Dock>
+      </div>
     </main>
   );
 }
