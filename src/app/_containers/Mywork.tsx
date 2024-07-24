3e60411a786a8a787a67ae5902c834e37f06fdde
hudy@mac-air <hudy9x@gmail.com>
2024-07-24 14:59:51 +0700

    update code

--- src/app/_containers/Mywork.tsx
+++ src/app/_containers/Mywork.tsx
@@ -1,6 +1,6 @@
 export default function Mywork() {
-  return <section className="section">
-    <button className="btn uppercase text-xs">My side projects</button>
+  return <section className="section mt-[100px]">
+    <button className="btn uppercase text-xs mb-[10px]">My side projects</button>
     <h2>Check out my work</h2>
     <p>I’ve worked on a variety of projects, from simple websites to complex web applications. Here are a few of my favorites.</p>
   </section>
