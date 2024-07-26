cc82bce95c1ff5c966a4f485fcd4123692246588
hudy9x <hudy9x@gmail.com>
2024-07-26 21:47:42 +0700

    refactor: update profile

--- src/app/_containers/Header.tsx
+++ src/app/_containers/Header.tsx
@@ -2,8 +2,7 @@ export default function Header() {
   return <header id="about" className="section flex items-start gap-7">
     <div>
       <h1>Hi, I am Hudy</h1>
-      <p>Web Developer turned Indie Hacker. I love
-        building things and sharing knowledges. Very active on X, Discord.</p>
+      <p>Obsessed with building products that people love. Indie hacker sharing the ups, downs, and everything in between on X and Discord.</p>
     </div>
 
     <img src="/avatar.png" className="w-16 h-16 sm:w-24 sm:h-24 rounded-full shadow" />
