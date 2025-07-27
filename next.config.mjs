fe93ab4222276f9a6c95afd03d97759b8cbec2fc
hudy@mac-air <hudy9x@gmail.com>
2025-07-27 21:49:42 +0700

    fix(build): accept avatar link from notion

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname:'s3-us-west-2.amazonaws.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
         hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
         pathname: '/**'
       },
